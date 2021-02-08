c08c3feb04da175c8b3b6c9bf7e4bf6b9e3cd170
Gavin Kijkul <gavinkijkul@gmail.com>
2021-02-08 16:13:09 +0700

    first commit for Sot5; added camera movement and zooming

---
+++ PlayerCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Camera myCamera;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float zoomSpeed;
+
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.up * cameraSpeed);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * cameraSpeed);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.down * cameraSpeed);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * cameraSpeed);
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            myCamera.orthographicSize -= zoomSpeed;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            myCamera.orthographicSize += zoomSpeed;
+        }
+    }
+}
